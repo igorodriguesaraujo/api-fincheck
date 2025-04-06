be59d902e5dc876d9f331ebb28475ee8f7f2cc6a
igorodriguesaraujo <igorodrigues_@hotmail.com>
2025-04-05 21:49:16 -0300

    refactor: Module user

--- src/modules/users/users.service.ts
+++ src/modules/users/users.service.ts
@@ -1,3 +1,4 @@
+import { Prisma } from '@prisma/client';
 /* eslint-disable prettier/prettier */
 import { Injectable } from '@nestjs/common';
 
@@ -6,4 +7,12 @@ import { UserRepository } from 'src/shared/database/repositories/users.repositor
 @Injectable()
 export class UsersService {
   constructor(private readonly userRepo: UserRepository) { }
+
+  async findByUnique(findUniqueDto: Prisma.UserFindUniqueArgs) {
+    return this.userRepo.findUnique(findUniqueDto);
+  }
+
+  async create(createUserDto: Prisma.UserCreateArgs) {
+    return this.userRepo.create(createUserDto);
+  }
 }
