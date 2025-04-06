129ce7e22fdc7bfb3a6ec897e42a3947fab0e8d4
igorodriguesaraujo <igorodrigues_@hotmail.com>
2025-04-06 16:34:52 -0300

    feat: Add message on validate SignInDtor

--- src/modules/auth/dto/signin.dto.ts
+++ src/modules/auth/dto/signin.dto.ts
@@ -2,12 +2,12 @@ import { IsEmail, IsNotEmpty, IsString, MinLength } from 'class-validator';
 
 export class SigninDto {
   @IsString()
-  @IsEmail()
-  @IsNotEmpty()
+  @IsEmail({}, { message: 'E-mail inválido' })
+  @IsNotEmpty({ message: 'Campo obrigatório' })
   email: string;
 
   @IsString()
-  @IsNotEmpty()
-  @MinLength(8)
+  @IsNotEmpty({ message: 'Campo obrigatório' })
+  @MinLength(8, { message: 'Campo deve ter no mínimo 8 caracteres' })
   password: string;
 }
