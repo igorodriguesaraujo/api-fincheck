bf5ac1073aba886d40a61cde380ae672a8263250
igorodriguesaraujo <igorodrigues_@hotmail.com>
2025-04-12 13:58:42 -0300

    fix:Organizando imports app.module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,17 @@
 /* eslint-disable prettier/prettier */
 import { Module } from '@nestjs/common';
-import { UsersModule } from './modules/users/users.module';
-import { DatabaseModule } from './shared/database/database.module';
-import { AuthModule } from './modules/auth/auth.module';
 import { APP_GUARD } from '@nestjs/core';
+
 import { AuthGuard } from './modules/auth/auth.guard';
+import { AuthModule } from './modules/auth/auth.module';
+
+import { UsersModule } from './modules/users/users.module';
+
 import { CategoriesModule } from './modules/categories/categories.module';
 import { BankAccountModule } from './modules/bank-account/bank-account.module';
 import { TransactionsModule } from './modules/transactions/transactions.module';
+
+import { DatabaseModule } from './shared/database/database.module';
 
 @Module({
   imports: [
